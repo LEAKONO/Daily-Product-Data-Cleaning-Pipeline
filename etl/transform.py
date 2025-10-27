8fcd99506cb2185ca879b5a40ae5f357301055af
LEAKONO <leakonoemmanuel3@gmail.com>
2025-10-27 22:42:01 +0300

    add transform logic for cleaning and sorting product data

---
+++ etl/transform.py
@@ -0,0 +1,16 @@
+# etl/transform.py
+import pandas as pd
+
+def clean_data(df):
+    print("🔧 Transforming data...")
+
+    df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
+
+    df = df.dropna(subset=["product_name", "price"])
+
+    df["price"] = pd.to_numeric(df["price"], errors="coerce")
+    df = df.dropna(subset=["price"])
+
+    df = df.sort_values(by="price", ascending=False)
+
+    return df
